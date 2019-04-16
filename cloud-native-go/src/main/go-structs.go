ae628c6137926613ee227c198997fcce5e406cce
dongzl <598363102@qq.com>
2019-04-16 19:43:57 +0800

    go struct

--- cloud-native-go/src/main/go-structs.go
+++ cloud-native-go/src/main/go-structs.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type power struct {
 	attack int
@@ -19,6 +21,53 @@ type nonPlayerCharacter struct {
 	hp int
 	power power
 	loc location
+}
+
+type attacker struct {
+	attackpower int
+	dmgbonus int
+}
+
+type sword struct {
+	attacker
+	twohanded bool
+}
+
+type gun struct {
+	attacker
+	bulletsremaining int
+}
+
+type weapon interface {
+	Wield() bool
+}
+
+func (s sword) Wield() bool  {
+	fmt.Println("You've wielded a sword!")
+	return true
+}
+
+func (g gun) Wield() bool {
+	fmt.Println("You've wielded a gun!")
+	return true
+}
+
+func wielder(w weapon) bool {
+	fmt.Println("Wielding...")
+	return w.Wield()
+}
+
+func (loc location) string() string  {
+	return fmt.Sprintf("(%f, %f, %f)", loc.x, loc.y, loc.z)
+}
+
+func (loc location) euclideanDistance(target location) float64 {
+	//return math.Sqrt((loc.x - target.x) * (loc.x - target.x) + (loc.y - target.y) * (loc.y - target.y) + (loc.z - target.z) * (loc.z - target.z))
+	return 0
+}
+
+func (npc nonPlayerCharacter) distanceTo(target nonPlayerCharacter) float64 {
+	return npc.loc.euclideanDistance(target.loc)
 }
 
 func main()  {
@@ -42,4 +91,16 @@ func main()  {
 	}
 
 	fmt.Println(anotherDemon)
+
+	sword1 := sword{attacker: attacker{attackpower: 1, dmgbonus: 5}, twohanded: true}
+	gun1 := gun{attacker: attacker{attackpower: 10, dmgbonus: 20}, bulletsremaining: 11}
+
+	fmt.Printf("Weapons: sword: %v, gun: %v\n", sword1, gun1)
+	sword1.Wield()
+	gun1.Wield()
+
+	wielder(sword1)
+	wielder(gun1)
+
+	fmt.Printf("Npc %v is %f units away from Npc %v\n", demon, demon.distanceTo(anotherDemon), anotherDemon)
 }
