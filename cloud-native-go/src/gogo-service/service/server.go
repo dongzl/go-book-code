623081e23b5666baa794e40cade3df11ac3909cb
dongzl <598363102@qq.com>
2019-04-19 09:47:22 +0800

    Chapter 5 section2

---
+++ cloud-native-go/src/gogo-service/service/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+	"net/http"
+)
+
+func NewServer() *negroni.Negroni  {
+	formatter := render.New(render.Options{IndentJSON: true })
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
+	return n
+}
+
+func initRoutes(mx *mux.Router, formatter *render.Render)  {
+	mx.HandleFunc("/test", testHandler(formatter)).Methods("GET")
+}
+
+func testHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct {
+			Test string
+		}{"This is a test"})
+	}
+}
