602ec488d3ab75894981f2ec6abdcb221b749470
Zonglei Dong <dongzonglei@apache.org>
2021-10-24 14:57:44 +0800

    get gushici main page data.

---
+++ go-shici/handle/shici_handle.go
@@ -0,0 +1,72 @@
+package handle
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"go-shici/gofish"
+	"io"
+	"strings"
+)
+
+var baseUrl = "https://so.gushiwen.cn"
+
+type AuthorHandle struct {
+	
+}
+
+func (h *AuthorHandle) Worker(body io.Reader, url string) {
+	doc, err := goquery.NewDocumentFromReader(body)
+
+	if err != nil {
+		fmt.Println("doc.err.", err)
+	}
+
+	doc.Find(".sons").Find(".cont").Find("a").Each(func(i int, selection *goquery.Selection) {
+		author := selection.Text()
+		fmt.Printf("%d author=%s\n", i, author)
+		link, _ := selection.Attr("href")
+		fmt.Printf("%d link=%s\n", i, link)
+
+		h := PomeHomeHandle{}
+		fish := gofish.NewGoFish()
+		request, err := gofish.NewRequest("GET", baseUrl + link, gofish.UserAgent, &h, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fish.Request = request
+		fish.Visit()
+	})
+}
+
+type PomeHomeHandle struct {
+
+}
+
+func (h *PomeHomeHandle) Worker(body io.Reader, url string) {
+	doc, err := goquery.NewDocumentFromReader(body)
+
+	if err != nil {
+		fmt.Println("doc.err.", err)
+	}
+
+	doc.Find(".sonspic").Find(".cont").Find("p").Find("a").Each(func(i int, selection *goquery.Selection) {
+		link, _ := selection.Attr("href")
+
+		if !strings.HasPrefix(link, "/shiwens") {
+			return
+		}
+
+		fmt.Println("作品主页=", baseUrl + link)
+
+		h := PomeInfoHandle{}
+		fish := gofish.NewGoFish()
+		request, err := gofish.NewRequest("GET", baseUrl + link, gofish.UserAgent, &h, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fish.Request = request
+		fish.Visit()
+	})
+}
