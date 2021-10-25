30e36f2c2b3dd52c3c986b2ba68f007cbf583c86
Zonglei Dong <dongzonglei@apache.org>
2021-10-25 11:01:43 +0800

    Add registry module.

---
+++ distributed/registry/server.go
@@ -0,0 +1,80 @@
+package registry
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+)
+
+const ServerPort = ":3000"
+const ServiceURL = "http://localhost" + ServerPort + "/services"
+
+type registry struct {
+	registrations []Registration
+	mutex         *sync.Mutex
+}
+
+func (r *registry) add(reg Registration) error {
+	r.mutex.Lock()
+	r.registrations = append(r.registrations, reg)
+	r.mutex.Unlock()
+	return nil
+}
+
+func (r *registry) remove(url string) error {
+	for i := range reg.registrations {
+		if reg.registrations[i].ServiceURL == url {
+			r.mutex.Lock()
+			reg.registrations= append(reg.registrations[:i], reg.registrations[:i + 1]...)
+			r.mutex.Unlock()
+			return nil
+		}
+	}
+	return fmt.Errorf("Service at URL %s not found", url)
+}
+
+var reg = registry{
+	registrations: make([]Registration, 0),
+	mutex:         new(sync.Mutex),
+}
+
+type RegistryService struct {
+}
+
+func (s RegistryService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("Request received")
+	switch r.Method {
+	case http.MethodPost:
+		dec := json.NewDecoder(r.Body)
+		var r Registration
+		err := dec.Decode(&r)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Adding service: %v with URL: %s\n", r.ServiceName, r.ServiceURL)
+		err = reg.add(r)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	case http.MethodDelete:
+		payload, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		url := string(payload)
+		log.Printf("Removing service ar URL: %s", url)
+		err = reg.remove(url)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
