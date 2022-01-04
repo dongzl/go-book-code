dfefd7dc38b33327b7803e0b3c8d4fefaab5e60b
Zonglei Dong <dongzonglei@apache.org>
2022-01-04 13:46:47 +0800

    Golang defer example.

---
+++ golangbot/29.defer/example04.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Naveen"
+	fmt.Printf("Orignal String: %s\n", string(name))
+	fmt.Printf("Reversed String: ")
+	for _, v := range []rune(name) {
+		defer fmt.Printf("%c", v)
+	}
+}
